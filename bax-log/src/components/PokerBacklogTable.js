2b2754bfa84fcf30d9bc0ab6beb7b853c37a912b
Kaan Adalılar <115321382+kaanadalilar@users.noreply.github.com>
2023-08-21 17:13:30 +0300

    Update PokerBacklogTable.js

--- bax-log/src/components/PokerBacklogTable.js
+++ bax-log/src/components/PokerBacklogTable.js
@@ -15,457 +15,115 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-
-
-
 import Tooltip from '@mui/material/Tooltip';
-
-
-
 import FormControlLabel from '@mui/material/FormControlLabel';
-
-
-
 import Switch from '@mui/material/Switch';
-
-
-
 import DeleteIcon from '@mui/icons-material/Delete';
-
-
-
 import FilterListIcon from '@mui/icons-material/FilterList';
-
-
-
 import { visuallyHidden } from '@mui/utils';
 
-
-
-
-
-
-
 function createData(status, head, description) {
-
-
-
     return {
-
-
-
         status,
-
-
-
         head,
-
-
-
         description,
-
-
-
     };
-
-
-
 }
-
-
-
-
-
-
 
 const rows = [
-
-
-
-    createData(305, 'active', 'kj'),
-
-
-
-    createData(452, 'passive', 'lazyy'),
-
-
-
-
-
+    createData("#305", "active", "Hop"),
+    createData("#452", "passive", "lazyy"),
 ];
 
-
-
-
-
-
-
 function descendingComparator(a, b, orderBy) {
-
-
-
     if (b[orderBy] < a[orderBy]) {
-
-
-
         return -1;
-
-
-
     }
-
-
 
     if (b[orderBy] > a[orderBy]) {
-
-
-
         return 1;
-
-
-
     }
-
-
-
     return 0;
-
-
-
 }
-
-
-
-
-
-
 
 function getComparator(order, orderBy) {
-
-
-
     return order === 'desc'
-
-
-
-        ? (a, b) => descendingComparator(a, b, orderBy)
-
-
-
-        : (a, b) => -descendingComparator(a, b, orderBy);
-
-
-
+        ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
 }
-
-
-
-
-
-
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-
-
-
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-
-
-
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-
-
-
-// with exampleArray.slice().sort(exampleComparator)
-
-
 
 function stableSort(array, comparator) {
 
-
-
     const stabilizedThis = array.map((el, index) => [el, index]);
-
-
-
     stabilizedThis.sort((a, b) => {
-
-
-
         const order = comparator(a[0], b[0]);
-
-
-
         if (order !== 0) {
-
-
-
             return order;
-
-
-
         }
-
-
-
         return a[1] - b[1];
-
-
-
     });
-
-
-
     return stabilizedThis.map((el) => el[0]);
-
-
-
 }
 
-
-
-
-
-
-
 const headCells = [
-
-
-
     {
-
-
-
         id: 'status',
-
-
-
         numeric: false,
-
-
-
         disablePadding: true,
-
-
-
         label: 'Status',
-
-
-
     },
-
-
-
     {
-
-
-
         id: 'heading',
-
-
-
         numeric: true,
-
-
-
         disablePadding: false,
-
-
-
         label: 'Task Heading',
-
-
-
     },
-
-
-
     {
-
-
-
         id: 'description',
-
-
-
         numeric: true,
-
-
-
         disablePadding: false,
-
-
-
         label: 'Task Description',
-
-
-
     },
-
-
-
-
-
 ];
 
-
-
-
-
-
-
 function EnhancedTableHead(props) {
-
-
-
     const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-
-
-
         props;
-
-
-
     const createSortHandler = (property) => (event) => {
-
-
-
         onRequestSort(event, property);
-
-
-
     };
 
-
-
-
-
-
-
     return (
-
-
-
         <TableHead>
-
-
-
             <TableRow>
-
-
-
                 <TableCell padding="checkbox">
-
-
-
                     <Checkbox
-
-
-
                         color="primary"
-
-
-
                         indeterminate={numSelected > 0 && numSelected < rowCount}
-
-
-
                         checked={rowCount > 0 && numSelected === rowCount}
-
-
-
                         onChange={onSelectAllClick}
-
-
-
                         inputProps={{
-
-
-
                             'aria-label': 'select all desserts',
-
-
-
                         }}
-
-
-
                     />
-
-
-
                 </TableCell>
 
-
-
                 {headCells.map((headCell) => (
-
-
-
                     <TableCell
-
-
-
                         key={headCell.id}
-
-
-
                         align={headCell.numeric ? 'right' : 'left'}
-
-
-
                         padding={headCell.disablePadding ? 'none' : 'normal'}
-
-
-
                         sortDirection={orderBy === headCell.id ? order : false}
-
-
-
                     >
-
-
-
                         <TableSortLabel
-
-
-
                             active={orderBy === headCell.id}
-
-
-
                             direction={orderBy === headCell.id ? order : 'asc'}
-
-
-
                             onClick={createSortHandler(headCell.id)}
-
-
-
                         >
-
-
-
                             {headCell.label}
-
-
-
                             {orderBy === headCell.id ? (
-
-
-
                                 <Box component="span" sx={visuallyHidden}>
-
-
-
                                     {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-
-
-
                                 </Box>
-
-
-
                             ) : null}
 
 
