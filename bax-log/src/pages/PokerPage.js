fb33dd700a2b53481f70ab46fdcdaf35120c172e
kubraaksux <hatice.aksu@boun.edu.tr>
2023-08-22 17:18:12 +0300

    people distributed around the table

--- bax-log/src/pages/PokerPage.js
+++ bax-log/src/pages/PokerPage.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import PokerTable from "../components/poker_table.jpg";
 import PokerBacklogTable from "../components/PokerBacklogTable";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import "./PokerPage.css";
 
 function PokerPage() {
@@ -15,35 +16,41 @@ function PokerPage() {
                 <Row>
                     <div className="intro-text">
                         <div>
-                            <div className="title">
-                                <h1 >Let's Play</h1>
-                            </div>
-
                             <div className="moonsun">
-
                                 <div className="moon">
                                     <PokerBacklogTable />
-
                                 </div>
-
-                                <div className="sun">
-
-                                    <img
-                                        src={PokerTable}
-                                        position="relative"
-
-                                    />
-
+                                <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+                                    <div style={{ width: 1277, height: 715, left: 365, top: 0, position: 'absolute', borderTopLeftRadius: 7.69, borderTopRightRadius: 30, borderBottomRightRadius: 30 }}>
+                                        <img style={{ width: 1277, height: 715, left: 0, top: 0, position: 'absolute', borderTopLeftRadius: 7.69 }} src={PokerTable} />
+                                        <div style={{ width: 72.40, height: 68.97, left: 492.70, top: 78.69, position: 'absolute' }}>
+                                            <div style={{ width: 60.33, height: 57.48, left: 6.03, top: 5.75, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        </div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 1047.74, top: 328.36, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon> </div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 212.16, top: 517.79, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 498.73, top: 579.97, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 714.92, top: 575.11, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 987.41, top: 517.79, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 151.83, top: 328.36, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 987.41, top: 147.66, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 212.16, top: 147.66, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 60.33, height: 57.48, left: 714.92, top: 84.52, position: 'absolute' }}> <AccountCircleIcon sx={{ fontSize: "60px", color: "white" }}></AccountCircleIcon></div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 480.63, top: 53.43, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Kaan</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 193.06, top: 113.66, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Tuna</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 194.06, top: 586.77, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>İbrahim</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 133.73, top: 293.38, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Kübra</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 698.83, top: 53.43, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Beyza</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 477.62, top: 641.17, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Olcay</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 698.83, top: 638.25, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Gökhan</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 969.31, top: 583.85, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Onur</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 1029.64, top: 290.47, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Hüseyin</div>
+                                        <div style={{ width: 95.52, height: 25.26, left: 969.31, top: 113.66, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Esin</div>
+                                    </div>
                                 </div>
-
-
-
                             </div>
-
 
 
                         </div>
-
                     </div>
                 </Row>
             </Container>
