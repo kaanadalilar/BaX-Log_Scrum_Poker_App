74cf565a40262ab0d3b1afb8185dba5a630a5ffd
TunaDagdanas <tdagdanas@gmail.com>
2023-08-28 16:52:16 +0300

    backlog task description apperance on top

--- bax-log/src/pages/PokerPage.js
+++ bax-log/src/pages/PokerPage.js
@@ -2,14 +2,58 @@ import React, { useEffect, useState } from "react";
 import { Button, Container, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import PokerTable from "../components/poker_table.jpg";
-import PokerBacklogTable from "../components/PokerBacklogTable";
 import UnderPokerTable from "../components/under_poker_table.png";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import "./PokerPage.css";
 import CountdownTimer from '../hooks/CountdownTimer';
 import AppService from '../AppService';
 
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+    { field: 'status', headerName: <strong>{'Status'}</strong>, width: 90 },
+    { field: 'header', headerName: <strong>{'Header'}</strong>, width: 90 },
+    { field: 'description', headerName: <strong>{'Description'}</strong>, width: 210 },
+];
+
+const rows = [
+    { id: 1, status: 'Active', header: '#234', description: "After game finishes, min, max, mode and average of the story points will appear on the screen." },
+    { id: 2, status: 'Passive', header: '#235', description: "Task description will be here 2" },
+    { id: 3, status: 'Passive', header: '#236', description: "Task description will be here 3" },
+    { id: 4, status: 'Active', header: '#237', description: "Task description will be here" },
+    { id: 5, status: 'Passive', header: '#238', description: "Task description will be here" },
+    { id: 6, status: 'Passive', header: '#584', description: "Task description will be here" },
+    { id: 7, status: 'Active', header: '#239', description: "Task description will be here" },
+    { id: 8, status: 'Passive', header: '#240', description: "Task description will be here" },
+    { id: 9, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 10, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 11, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 12, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 13, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 14, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 15, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 16, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 17, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 18, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 19, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 20, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 21, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 23, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 24, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 25, status: 'Active', header: '#234', description: "Task description will be here" },
+    { id: 26, status: 'Passive', header: '#584', description: "Task description will be here" },
+    { id: 27, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 28, status: 'Passive', header: '#234', description: "Task description will be here" },
+    { id: 29, status: 'Passive', header: '#234', description: "Task description will be here" },
+];
+
+
 function PokerPage() {
+
+    const [task, setTask] = React.useState('');
+    const handleRowClick = (params) => {
+        setTask(`${params.row.description}`);
+    };
 
     const updateUserPickCard = (chName, chPickedCard, chIsPickedCard) => {
         let user = { name: chName, pickedCard: chPickedCard, isPickedCard: chIsPickedCard };
@@ -205,7 +249,19 @@ function PokerPage() {
                         <div>
                             <div className="moonsun">
                                 <div className="moon">
-                                    <PokerBacklogTable />
+                                    <div style={{ height: 860, width: '100%' }}>
+                                        <DataGrid
+                                            rows={rows}
+                                            columns={columns}
+                                            initialState={{
+                                                pagination: {
+                                                    paginationModel: { page: 0, pageSize: 5 },
+                                                },
+                                            }}
+                                            pageSizeOptions={[5, 10, 15, 20, 25]}
+                                            onRowClick={handleRowClick}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="sun" style={{ width: '100%', height: '100%', position: 'relative' }}>
                                     <div style={{ width: 1477, height: 715, left: 0, top: 0, position: 'absolute', borderTopLeftRadius: 7.69, borderTopRightRadius: 30, borderBottomRightRadius: 30 }}>
@@ -483,7 +539,7 @@ function PokerPage() {
                                         <div style={{ width: 19.23, height: 19.23, position: 'relative' }}>
                                             <div style={{ width: 11.22, height: 11.22, left: 4.01, top: 4.01, position: 'absolute', border: '0.80px white solid' }}></div>
                                         </div>
-                                        <div style={{ width: 867, color: 'white', fontSize: 13.46, fontFamily: 'Inter', fontWeight: '500', lineHeight: 19.23, wordWrap: 'break-word' }}>TASK WILL APPEAR HERE </div>
+                                        <div style={{ width: 867, color: 'white', fontSize: 13.46, fontFamily: 'Inter', fontWeight: '500', lineHeight: 19.23, wordWrap: 'break-word' }}> {task} </div>
                                         <div style={{ width: 1, height: 5, position: 'relative' }} />
                                     </div>
                                 </div>
