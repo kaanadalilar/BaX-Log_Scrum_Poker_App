eb584c99547c8986839df7b46c9d0efb7875514c
Kaan Adalılar <115321382+kaanadalilar@users.noreply.github.com>
2023-09-11 16:22:35 +0300

    Static color deleted

--- bax-log/src/pages/PokerPage.js
+++ bax-log/src/pages/PokerPage.js
@@ -715,7 +715,7 @@ function PokerPage() {
                                             {(sessionUsers.length > 8) && (
                                                 <div style={{ width: 43.50, height: 56.35, left: 390, top: 280, position: 'absolute', transform: 'rotate(-45deg)', transformOrigin: '0 0' }}>
                                                     <div style={{ width: 43.50, height: 56.35, left: 0, top: 0, position: 'absolute' }}>
-                                                        <div style={{ backgroundColor: (sendSucceeded) ? 'rgba(255, 0, 0, 0.8)' : "white", width: 43.50, height: 56.35, left: 0, top: 0, position: 'absolute', boxShadow: '0px 5px 34px rgba(0, 0, 0, 0.10)', border: '0.50px #D2D2D2 solid' }} />
+                                                        <div style={{ width: 43.50, height: 56.35, left: 0, top: 0, position: 'absolute', background: '#F2F2F2', boxShadow: '0px 5px 34px rgba(0, 0, 0, 0.10)', border: '0.50px #D2D2D2 solid' }} />
                                                         <div style={{ width: 19, height: 19, left: 0, top: 0.45, position: 'absolute', textAlign: 'center', color: '#F24822', fontSize: 30, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word' }}>A</div>
                                                     </div>
                                                     <div style={{ width: 26, height: 34, left: 18, top: 30, position: 'absolute' }}>
